5dfec180e65cd1c737f008971a7a122a21f20f97
Hussein Nasrallah <109015272+Husein81@users.noreply.github.com>
2025-04-26 14:01:33 +0300

    set api

---
+++ api/env.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    PORT: string;
+    NODE_ENV: "development" | "production" | "test";
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  }
+}
