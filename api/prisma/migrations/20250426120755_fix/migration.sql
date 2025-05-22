21d09be059efcf9a1c72842ed6b66dd3ebd96e03
Hussein Nasrallah <109015272+Husein81@users.noreply.github.com>
2025-05-22 09:11:37 +0300

    Delete api/prisma directory

--- api/prisma/migrations/20250426120755_fix/migration.sql
+++
@@ -1,8 +0,0 @@
--- AddForeignKey
-ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Expense" ADD CONSTRAINT "Expense_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Category" ADD CONSTRAINT "Category_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- api/prisma/migrations/20250520155848_modify_ammount_balance/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Account" ALTER COLUMN "balance" SET DATA TYPE TEXT;
-
--- AlterTable
-ALTER TABLE "Expense" ALTER COLUMN "amount" SET DATA TYPE TEXT;
