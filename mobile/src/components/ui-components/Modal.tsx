2813f5ec03722c6c0a59ff76ada13b60d672f79f
Husein81 <husseinnasrallah2002@gmail.com>
2025-05-26 22:23:34 +0300

    modify modal

--- mobile/src/components/ui-components/Modal.tsx
+++ mobile/src/components/ui-components/Modal.tsx
@@ -1,4 +1,10 @@
-import { Modal as RModal, ScrollView, View } from "react-native";
+import {
+  Pressable,
+  Modal as RModal,
+  SafeAreaView,
+  ScrollView,
+  View,
+} from "react-native";
 
 // local imports
 import { cn } from "~/lib/utils";
@@ -12,39 +18,55 @@ import { Icon } from "~/lib/icons/Icon";
 import { useColorScheme } from "~/lib/useColorScheme";
 
 export const Modal = () => {
-  const { open, title, body, onClose } = useModalStore();
+  const { open, title, body, transparent, onClose } = useModalStore();
   const { isDarkColorScheme } = useColorScheme();
 
   return (
     <RModal
       visible={open}
+      transparent={transparent}
       presentationStyle={"formSheet"}
       onRequestClose={onClose}
-      animationType={"slide"}
+      animationType={transparent ? "fade" : "slide"}
     >
       <GestureHandlerRootView style={{ flex: 1 }}>
         <BottomSheetModalProvider>
-          <View
+          <Pressable
+            onPress={transparent ? onClose : undefined}
             className={cn(
-              "flex-1 gap-4 p-8 items-center justify-center bg-background"
+              "flex-1 bg-background p-6",
+              transparent &&
+                "bg-background/65 backdrop-blur justify-center items-center"
             )}
           >
-            {title && (
-              <View className="w-full flex-row items-center justify-center">
-                <Icon
-                  name="ChevronLeft"
-                  size={28}
-                  onPress={onClose}
-                  className="absolute left-0 bg-primary  rounded-lg"
-                  color={isDarkColorScheme ? "#000" : "#fff"}
-                />
-                <Text className="text-2xl font-semibold ">{title}</Text>
-              </View>
-            )}
-            <ScrollView className="w-full" showsVerticalScrollIndicator={false}>
-              {body}
-            </ScrollView>
-          </View>
+            <View
+              className={cn(
+                transparent
+                  ? "bg-card rounded-xl p-6 w-[90%] max-h-[80%]"
+                  : "flex-1",
+                "gap-4"
+              )}
+            >
+              {title && (
+                <View className="w-full flex-row items-center justify-center">
+                  <Icon
+                    name="ChevronLeft"
+                    size={28}
+                    onPress={onClose}
+                    className="absolute left-0 bg-primary  rounded-lg"
+                    color={isDarkColorScheme ? "#000" : "#fff"}
+                  />
+                  <Text className="text-2xl font-semibold ">{title}</Text>
+                </View>
+              )}
+              <ScrollView
+                className="w-full"
+                showsVerticalScrollIndicator={false}
+              >
+                {body}
+              </ScrollView>
+            </View>
+          </Pressable>
         </BottomSheetModalProvider>
       </GestureHandlerRootView>
     </RModal>
